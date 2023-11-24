4e664af326bc9913e458ab8f208b78b3c1edbbca
Joakimcb <cewers@net.chalmers.se>
2023-11-24 13:47:19 +0100

    Definently our code
    kör examples för att plotta träd

---
+++ examples.py
@@ -0,0 +1,60 @@
+from grammar import *
+from plot import *
+
+# Grammar
+
+def algae():
+	system = LSystem('A', {'A': 'AB', 'B': 'A'})
+	for _ in range(8):
+		print(system.state)
+		system.step()
+
+# 2D plots
+
+def koch():
+	system = LSystem('F', {'F': 'F+F-F-F+F'})
+	for _ in range(5):
+		system.step()
+	plot2d(system.state, 90)
+
+def tree():
+	system = LSystem('B', {'B': 'gF/[B]+B*', 'g': 'b'})
+	for _ in range(7):
+		system.step()
+		plot2d(system.state, 20, 1.2, np.array([0, 1.]), {'g': 'g', 'b': '#654321'})
+	plt.show()
+	#plot2d(system.state, 20, 1.2, np.array([0, 1.]), {'g': 'g', 'b': '#654321'})
+def tree2():
+	system = LSystem('X', {'F': 'FF', 'X':'F-[[X]+X]+F[+FX]-X'})
+	for _ in range(6):
+		system.step()
+	plot2d(system.state, 22.5, 1.2, np.array([0, 1.]), {'g': 'g', 'b': '#654321'})
+	
+def plant():
+	system = LSystem('gX', {'X': 'F+[[X]-X]-F[-FX]+X', 'F': 'FF'})
+	for _ in range(5):
+		system.step()
+	plot2d(system.state, 25, 1, rotate(np.array([1., 0]), 50), {'g': 'g'})
+
+def liana():
+	system = LSystem('gF', {'F': 'F[+F]F[-F]F'})
+	for _ in range(5):
+		system.step()
+	plot2d(system.state, 25, 1, np.array([0, 1.]), {'g': 'g'})
+
+# 3D plots
+
+def tree3d():
+	system = LSystem('B', {'B': 'gF/[+B][-B][&B]^B*', 'g': 'b'})
+	for _ in range(6):
+		system.step()
+	plot3d(system.state, 20, 1.2, np.array([0, 0, 1.]), {'g': 'g', 'b': '#654321'})
+
+def pine3d():
+	system = LSystem('B', {'B': 'gF/[+++B][---B][&&&B][^^^B]*gF/[++B][--B][&&B][^^B]*gF/B*', 'g': 'b'})
+	for _ in range(4):
+		system.step()
+	plot3d(system.state, 20, 2, np.array([0, 0, 1.]), {'g': 'g', 'b': '#654321'})
+
+if __name__ == '__main__':
+	liana()
